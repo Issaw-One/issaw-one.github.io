d442b6da47b1849858983522b13f419f83b47933
Issaw-One <bobcabrao@gmail.com>
2025-05-13 11:01:11 +0200

    Create torrent.php

---
+++ torrent.php
@@ -0,0 +1,300 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="List Of Streaming Websites">
+    <meta name="author" content="Issaw">
+
+
+    <title>Streaming Websites List</title>
+
+
+    <!--ressources externes-->
+    <link href="/style/bootstrap.min.css" rel="stylesheet">
+    <script src="/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="/style/animate.min.css">
+
+    <!-- Google Tag Manager -->
+    <script>(function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start':
+                    new Date().getTime(), event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s), dl = l !== 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-5C29DQLP');</script>
+    <!-- End Google Tag Manager -->
+
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+
+    <!--    ressources internes -->
+    <link rel="stylesheet" type="text/css" href="https://recherche-stream.fr/ressources/style/barre1.css">
+    <link rel="stylesheet" type="text/css" href="https://recherche-stream.fr/ressources/style/style.css">
+    <link rel="stylesheet" type="text/css" href="https://recherche-stream.fr/ressources/style/website/manager.css">
+    <script defer src="https://recherche-stream.fr/ressources/js/theme.js"></script>
+    <script defer src="js/main2.js"></script>
+    <script src="https://recherche-stream.fr/ressources/js/ressources/scrollE2.js"></script>
+    <script defer src="https://recherche-stream.fr/ressources/js/website/manager.js"></script>    <script type="text/javascript">
+        let siteBase = "https://recherche-stream.fr/.";
+        let apiBase = siteBase + "/api/"
+    </script>
+
+</head>
+
+<body>
+
+<header>
+    
+</header>
+
+
+
+
+<div class="darkWhite">
+    <button onclick="dark_white_mode()">
+        <img src="https://recherche-stream.fr/ressources/images/icons/dark.webp" id="mode"
+             alt="dark-theme recherche-stream" class="display-light">
+
+        <img src="https://recherche-stream.fr/ressources/images/icons/light.webp" id="mode"
+             alt="lighttheme recherche-stream" class="display-dark">
+
+    </button>
+</div>
+
+
+
+
+<div class="container mt-5">
+    <div class="text-center mb-4">
+        <h1>Streaming Websites List</h1>
+
+        <h4>use <a target="_blank" rel="nofollow" class="t1" href="https://ublockorigin.com/">uBlock Origin</a> to block ads</h4>
+
+        <div class="m-3 formsearchsite">
+            <input type="text" id="searchSite" placeholder="rechercher un site de streaming" class="form-control">
+        </div>
+
+        
+    </div>
+    <div class="row">
+        
+
+                    
+                    
+            <div class="col-12 my-4 helptosearch">
+                <div class="p-4 bg-light rounded text-center">
+                    <h2 class="mb-4 text-dark">Téléchargement de Torrent</h2>
+                </div>
+            </div>
+            
+
+            <div class="col-md-6 col-lg-4 mt-4 website">
+                        <a class="" target="_blank" rel="nofollow" href="https://thepiratebay10.info/">
+                            <div class="card h-100">
+                            <div class="wallapp">
+                                <img class="img-fluid" 
+                                     src="images/logoSites/tpb.webp" 
+                                     alt="logo de allostreaming" loading="lazy">
+                            </div>
+
+ 
+                                <div class="card-footer text-center mt-0 p-3">
+                                    <h3>The Pirate Bay</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    
+            
+                    <div class="col-md-6 col-lg-4 mt-4 website">
+                        <a class="" target="_blank" rel="nofollow" href="https://1337x.to/">
+                            <div class="card h-100">
+                            <div class="wallapp">
+                                <img class="img-fluid" 
+                                     src="images/logoSites/logo.svg" 
+                                     alt="logo de allostreaming" loading="lazy">
+                            </div>
+
+ 
+                                <div class="card-footer text-center mt-0 p-3">
+                                    <h3>1337X</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+
+                    <div class="col-md-6 col-lg-4 mt-4 website">
+                        <a class="" target="_blank" rel="nofollow" href="https://www.oxtorrent3.com/home">
+                            <div class="card h-100">
+                            <div class="wallapp">
+                                <img class="img-fluid" 
+                                     src="images/logoSites/oxtorrent.webp" 
+                                     alt="logo de allostreaming" loading="lazy">
+                            </div>
+
+ 
+                                <div class="card-footer text-center mt-0 p-3">
+                                    <h3>OxTorrent</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+
+                    <div class="col-md-6 col-lg-4 mt-4 website">
+                        <a class="" target="_blank" rel="nofollow" href="https://www.yggtorrents.top/home">
+                            <div class="card h-100">
+                            <div class="wallapp">
+                                <img class="img-fluid" 
+                                     src="images/logoSites/ygg.svg" 
+                                     alt="logo de allostreaming" loading="lazy">
+                            </div>
+
+ 
+                                <div class="card-footer text-center mt-0 p-3">
+                                    <h3>YggTorrent</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+
+                    <div class="col-md-6 col-lg-4 mt-4 website">
+                        <a class="" target="_blank" rel="nofollow" href="https://www.cpasbien4.com/home">
+                            <div class="card h-100">
+                            <div class="wallapp">
+                                <img class="img-fluid" 
+                                     src="images/logoSites/cpasbien.webp" 
+                                     alt="logo de allostreaming" loading="lazy">
+                            </div>
+
+ 
+                                <div class="card-footer text-center mt-0 p-3">
+                                    <h3>Cpasbien</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+
+                    <div class="col-md-6 col-lg-4 mt-4 website">
+                        <a class="" target="_blank" rel="nofollow" href="https://www.zetorrents1.com/home">
+                            <div class="card h-100">
+                            <div class="wallapp">
+                                <img class="img-fluid" 
+                                     src="images/logoSites/zetorrents.webp" 
+                                     alt="logo de allostreaming" loading="lazy">
+                            </div>
+
+ 
+                                <div class="card-footer text-center mt-0 p-3">
+                                    <h3>Zetorrents</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+
+                    <div class="col-md-6 col-lg-4 mt-4 website">
+                        <a class="" target="_blank" rel="nofollow" href="https://torrentz9.org/">
+                            <div class="card h-100">
+                            <div class="wallapp">
+                                <img class="img-fluid" 
+                                     src="images/logoSites/torrentz.svg" 
+                                     alt="logo de allostreaming" loading="lazy">
+                            </div>
+
+ 
+                                <div class="card-footer text-center mt-0 p-3">
+                                    <h3>Torrentz9</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+
+                    <div class="col-md-6 col-lg-4 mt-4 website">
+                        <a class="" target="_blank" rel="nofollow" href="https://www.limetorrents.lol/">
+                            <div class="card h-100">
+                            <div class="wallapp">
+                                <img class="img-fluid" 
+                                     src="images/logoSites/limetorrents.webp" 
+                                     alt="logo de allostreaming" loading="lazy">
+                            </div>
+
+ 
+                                <div class="card-footer text-center mt-0 p-3">
+                                    <h3>LimeTorrents</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+
+                    <div class="col-md-6 col-lg-4 mt-4 website">
+                        <a class="" target="_blank" rel="nofollow" href="https://binge-browser.vercel.app/">
+                            <div class="card h-100">
+                            <div class="wallapp">
+                                <img class="img-fluid" 
+                                     src="images/logoSites/binge.ico" 
+                                     alt="logo de allostreaming" loading="lazy">
+                            </div>
+
+ 
+                                <div class="card-footer text-center mt-0 p-3">
+                                    <h3>Binge Browser</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+
+                    <div class="col-md-6 col-lg-4 mt-4 website">
+                        <a class="" target="_blank" rel="nofollow" href="https://nyaa.land/">
+                            <div class="card h-100">
+                            <div class="wallapp">
+                                <img class="img-fluid" 
+                                     src="images/logoSites/nyaa.png" 
+                                     alt="logo de allostreaming" loading="lazy">
+                            </div>
+
+ 
+                                <div class="card-footer text-center mt-0 p-3">
+                                    <h3>Nyaa</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    
+                    
+                    <footer class="footer" style="margin-top:40px;">
+			
+                        <div class="container">
+                            <p style="text-align:center;font-size:16px;"><a href="#">Back to top</a></p>
+                        </div>
+                    </footer>         
+            
+            
+    </div>
+
+</div>
+
+
+
+
+
+
+
+<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script defer src="https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015" integrity="sha512-ZpsOmlRQV6y907TI0dKBHq9Md29nnaEIPlkf84rnaERnq6zvWvPUqr2ft8M1aS28oN72PdrCzSjY4U6VaAw1EQ==" data-cf-beacon='{"rayId":"92e29223ac43e1e8","version":"2025.3.0","r":1,"token":"20257167efd0408eb555de46e3bc887d","serverTiming":{"name":{"cfExtPri":true,"cfL4":true,"cfSpeedBrain":true,"cfCacheStatus":true}}}' crossorigin="anonymous"></script>
+</body>
+</html>
+
