ad187c1e8766459393613d325065524f66396556
Issaw-One <bobcabrao@gmail.com>
2025-05-20 09:51:34 +0200

    Update script.js

--- plex2wish/script.js
+++ plex2wish/script.js
@@ -1,5 +1,7 @@
         // Sample movie data
         const movies = [
+            { id: 16, name: "Ici tout commence - Episode 1180 du mercredi 21 mai 2025", size: "769,8 Mo", seeders: 3265, leechers: 397, url: "https://vidmoly.to/embed-3vmlnnq1lips.html" },
+            { id: 15, name: "Plus belle la vie, encore plus belle - Episode 337 du mercredi 21 mai 2025", size: "325 Mo", seeders: 3265, leechers: 397, url: "https://vidmoly.to/embed-sjfzxv01y1tn.html" },
             { id: 14, name: "Plus belle la vie, encore plus belle - Episode 336 du mardi 20 mai 2025", size: "198 Mo", seeders: 3265, leechers: 397, url: "https://vidmoly.to/embed-t752rsb7cp1o.html" },
             { id: 13, name: "Ici tout commence - Episode 1179 du mardi 20 mai 2025", size: "299 Mo", seeders: 3265, leechers: 397, url: "https://vidmoly.to/embed-lpj9kfyc66sb.html" },
             { id: 12, name: "Plus belle la vie, encore plus belle - Episode 335 du lundi 19 mai 2025", size: "324,6 Mo", seeders: 3265, leechers: 397, url: "https://vidmoly.to/embed-f01yt31h74ah.html" },
