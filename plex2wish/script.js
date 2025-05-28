32954c1c543f8d2f22de6e535611b2a6c504154b
Issaw-One <bobcabrao@gmail.com>
2025-05-29 00:12:43 +0200

    Update script.js

--- plex2wish/script.js
+++ plex2wish/script.js
@@ -1,5 +1,7 @@
         // Sample movie data
         const movies = [
+            { id: 30, name: "Ici tout commence - Episode 1187 du vendredi 30 mai 2025", size: "", seeders: 3265, leechers: 397, url: "" },
+            { id: 29, name: "Plus belle la vie, encore plus belle - Episode 344 du vendredi 30 mai 2025", size: "", seeders: 3265, leechers: 397, url: "https://voe.sx/e/zdsvttdbjghx" }, //105
             { id: 28, name: "Ici tout commence - Episode 1186 du jeudi 29 mai 2025", size: "", seeders: 3265, leechers: 397, url: "https://voe.sx/e/falnbij26jx2" },
             { id: 27, name: "Plus belle la vie, encore plus belle - Episode 343 du jeudi 29 mai 2025", size: "", seeders: 3265, leechers: 397, url: "https://jilliandescribecompany.com/e/grgv3l08eg27" }, //104
             { id: 26, name: "Ici tout commence - Episode 1185 du mercredi 28 mai 2025", size: "", seeders: 3265, leechers: 397, url: "https://voe.sx/e/4yzlq60wwemg" },
